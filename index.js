8b99e2fc7f62f9102a8d7504c29783521eb6b72a
Adeboye Joshua <adeboyeayodele2011@gmail.com>
2022-12-22 05:13:40 +0100

    When i created modal and form

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+//Host: https://ayodele1245.github.io/StudentRecord.github.io/
+
+// Get the modal
+var modal = document.getElementById('modal');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
 let monthSelect=document.getElementById("month");
 let weekSelect=document.getElementById("week");
 
+//Populating the months in select button
 let month=["December", "January", "February", "March", "April", "May", "June"];
 function populateMonth(){
     for (let i = 0; i < month.length; i++) {
@@ -11,7 +25,7 @@ function populateMonth(){
     }
 };
 populateMonth();
-
+//Populating the weeks in the select button
 let week=["Week 1", "Week 2", "Week 3", "Week 4"];
 function populateWeek(){
     for (let i = 0; i < week.length; i++) {
